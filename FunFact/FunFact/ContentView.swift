c2a4b0d0f471558242e27eea2033ed40862ed5d9
Naz1380 <he4687@wayne.edu>
2025-02-22 12:17:30 -0500

    Initial commit - Xcode project setup

---
+++ FunFact/FunFact/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FunFact
+//
+//  Created by Nazanin Mahmoudi on 2/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FunFact/FunFact/FunFactApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunFactApp.swift
+//  FunFact
+//
+//  Created by Nazanin Mahmoudi on 2/22/25.
+//
+
+import SwiftUI
+
+@main
+struct FunFactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
